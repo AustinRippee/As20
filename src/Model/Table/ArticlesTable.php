69b4e8805ba28a19f93c38d590c0e61499458381
Austin Rippee <austinrippee@gmail.com>
2021-04-14 11:45:12 -0400

    3rd Commit

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -1,9 +1,12 @@
 <?php
 
-use \Cake\ORM\Table;
+namespace App\Model\Table;
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
 
-class ArticlesTable extends Table{
+class ArticlesTable extends Table {
     public function initialize (array $config) : void {
-        $this->addBehavior('Timestamp'); // Automatically populates timestamp (datetime) fields
+        $this->addBehavior('Timestamp'); // automatically populates timestamp (datetime) fields
     }
 }
